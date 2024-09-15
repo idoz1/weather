038e89521a227e00f0415857e1ef7ffac6f31f43
idoz1 <169044973+idoz1@users.noreply.github.com>
2024-09-15 19:15:00 +0300

    dynamic background

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 const API_KEY = "967c67c047f4249b6c55f6803ab1324d"
+const API_KEY_BG = "3xb91jD8twEapM2aQaG3UxihCqStNzROJ9WmKnbad20"
 const CITY_INFO = document.querySelector('#cityInfo')
 const LOADING_ELEM = document.querySelector('#loading')
 const SEARCH_FORM = document.forms[0];
 const ERROR_FIELD = document.querySelector('#errorMessage')
+const BACKGROUND = document.querySelector('main')
 
 const CITY_INFO_FIELDS_MAP = {
   name: document.querySelector('#cityName'),
@@ -17,6 +19,7 @@ const CITY_INFO_FIELDS_MAP = {
   wind: document.querySelector('#wind'),
 }
 
+let emptyBackground = true
 let loadingState = false;
 
 SEARCH_FORM.addEventListener('submit', async function() {
@@ -27,10 +30,15 @@ SEARCH_FORM.addEventListener('submit', async function() {
   toggleLoading()
 
   const requestInfo = await fetchWeather(this.elements.city.value)
+  const requestBackground = await fetchBackground(this.elements.city.value)
 
   if (+requestInfo.cod === 200){
+    emptyBackground = false
+    renderBackground(requestBackground)
     renderData(requestInfo)
   } else {
+    emptyBackground = true
+    renderBackground()
     handleError(requestInfo)
   }
 
@@ -46,9 +54,9 @@ function toggleLoading() {
 }
 
 async function fetchBackground(img) {
-    const res = await fetch(``)
-    const data = await res.json()
-    return data
+  const res = await fetch(`https://api.unsplash.com/search/photos?client_id=${API_KEY_BG}&query=${img}&per_page=1`);
+  const data = await res.json();
+  return data;
 }
 
 async function fetchWeather(city) {
@@ -58,6 +66,7 @@ async function fetchWeather(city) {
 }
 
 function renderData(info) {
+
   CITY_INFO_FIELDS_MAP.name.innerText = info.name
   CITY_INFO_FIELDS_MAP.clouds.innerText = info.clouds.all + '%'
   CITY_INFO_FIELDS_MAP.wind.innerText = info.wind.speed + " m/s"
@@ -72,6 +81,15 @@ function renderData(info) {
   CITY_INFO_FIELDS_MAP.icon.setAttribute('alt', info.weather[0].description)
 
   CITY_INFO.classList.remove('hidden')
+}
+
+function renderBackground(data) {
+  if (data && data.results && data.results.length > 0 & emptyBackground === false) {
+    const imageUrl = data.results[0].urls.raw;  
+    BACKGROUND.classList.add(`bg-[url(${imageUrl})]`);
+  }else {
+    BACKGROUND.classList.add(`bg-[url(https://static.vecteezy.com/system/resources/thumbnails/023/232/609/original/temporary-closed-bw-404-animation-hanging-signboard-empty-state-4k-concept-footage-with-alpha-channel-transparency-monochromatic-error-flash-message-for-web-page-not-found-ui-design-video.jpg)]`);
+  }
 }
 
 function handleError(err) {
